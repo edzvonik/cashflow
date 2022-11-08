b77d7d86ee7c148388f84eabf3f592ee4230b96f
Evgenii Dzvonik <ev.dzvonik@gmail.com>
2022-11-08 11:12:38 +0300

    Refactor last changeset

--- src/main/resources/db/changelog/changes/2022-11-08-change_accountId_categoryId_in_transaction.sql
+++ src/main/resources/db/changelog/changes/2022-11-08-change_accountId_categoryId_in_transaction.sql
@@ -1,2 +1,2 @@
-alter table transaction alter column accountId drop not null;
-alter table transaction alter column categoryId drop not null;
+alter table transaction alter column account_id drop not null;
+alter table transaction alter column category_id drop not null;
