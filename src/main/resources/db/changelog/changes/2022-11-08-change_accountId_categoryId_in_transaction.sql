f10792534edcbe5a6552ad3a0745dc323700cced
edzvonik <ev.dzvonik@gmail.com>
2023-01-19 09:18:34 +0300

    Refactor packages structure

--- src/main/resources/db/changelog/changes/2022-11-08-change_accountId_categoryId_in_transaction.sql
+++
@@ -1,6 +0,0 @@
---liquibase formatted sql
-
---changeset edzvonik:3
-
-alter table transaction alter column account_id drop not null;
-alter table transaction alter column category_id drop not null;
