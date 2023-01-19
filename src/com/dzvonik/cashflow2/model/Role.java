25d1ed69402c1bb9381c405a3d8dc59e199a9878
edzvonik <ev.dzvonik@gmail.com>
2023-01-19 09:19:11 +0300

    Implement Role model

--- src/com/dzvonik/cashflow2/model/Role.java
+++ src/com/dzvonik/cashflow2/model/Role.java
@@ -1,5 +1,31 @@
-package main.java.com.dzvonik.cashflow2.model;
+package com.dzvonik.cashflow2.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString
+@EqualsAndHashCode
+@Table(name = "roles")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Role {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
 
 }
--- src/com/dzvonik/cashflow2/model/User.java
+++ src/com/dzvonik/cashflow2/model/User.java
@@ -1,13 +1,7 @@
 package com.dzvonik.cashflow2.model;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
+import java.util.List;
+import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -16,10 +10,19 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 
 @Entity
 @Getter
@@ -31,25 +34,31 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
-    @Id
-    @SequenceGenerator(name = "seq_user", sequenceName = "seq_user")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_user")
-    private Long id;
+  @Id
+  @SequenceGenerator(name = "seq_user", sequenceName = "seq_user")
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_user")
+  private Long id;
 
-    @Column(nullable = false)
-    private String name;
+  @Column(nullable = false)
+  private String name;
 
-    @Column(nullable = false)
-    private String email;
+  @Column(nullable = false)
+  private String email;
 
-    @Column(nullable = false)
-    private String baseCurrency;
+  @Column(nullable = false)
+  private String baseCurrency;
 
-    @OneToMany(
-            fetch = FetchType.LAZY,
-            cascade = CascadeType.ALL,
-            orphanRemoval = true)
-    @JoinColumn(name = "user_id")
-    private List<Account> accounts;
+  @OneToMany(
+      fetch = FetchType.LAZY,
+      cascade = CascadeType.ALL,
+      orphanRemoval = true)
+  @JoinColumn(name = "user_id")
+  private List<Account> accounts;
+
+  @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+  @JoinTable(name = "user_roles",
+      joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+      inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+  private Set<Role> roles;
 
 }
